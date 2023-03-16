6ba514245d61df4bbde3bf46412e491f7bce9697
Santiago <santiago.s.sira@gmail.com>
2023-03-16 14:41:36 -0700

    Changed the Project Model in the Database

--- portfolio/models.py
+++ portfolio/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Project(models.Model):
     project_name = models.CharField(max_length=30)
     project_type = models.CharField(max_length=20)
+    project_image = models.ImageField(upload_to= 'portfolio', blank=True, null=True)
 
     def __str__(self):
         return self.project_name
